da91229c82541199505b9df39496a82a51f5b5d4
Rishav <kumarrishav690@gmail.com>
2020-04-24 16:01:04 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAppandroid()
+	{
+		System.out.println("NativeAppandroid");
+	}
+	
+	@Test
+	public void IOApps()
+	{
+		System.out.println("IOApps");
+	}
+}
